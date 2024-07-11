9fac95c3232071031393a2774fb96f0edb2043a9
TeodorNTNU <teodorrk@ntnu.no>
2024-07-11 10:08:34 +0200

    Kjøøør da

--- my-fireworks-app/app/server.py
+++ my-fireworks-app/app/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
-from retrieval_agent_fireworks import chain as retrieval_agent_fireworks_chain
+from packages.retrieval_agent_fireworks.chain import agent_executor as retrieval_agent_fireworks_chain
 
 app = FastAPI()
 
@@ -11,11 +11,9 @@ async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
 
-# Edit this to add the chain you want to add
 add_routes(app, retrieval_agent_fireworks_chain, path="/retrieval-agent-fireworks")
-
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
